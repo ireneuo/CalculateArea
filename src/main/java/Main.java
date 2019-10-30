1065a8934ff48cbdb4f7b660c17f382c81067bcf
ireneuo <hyperenigmatic@gmail.com>
2019-10-30 21:49:03 +0100

    Rectangle + Main, it works :)

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle rectangleArea = new Rectangle();
+
+        System.out.println("pole prostokata wynosi: "+ rectangleArea.calculateArea());
+    }
+}
